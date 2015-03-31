e4e393c082f65fbf8ddb8a9228d17b6b75940ce8
Justin Buchanan <justbuchanan@gmail.com>
2015-03-31 19:42:48 -0400

    junk

--- modeling/justin/Robot.m
+++ modeling/justin/Robot.m
@@ -4,11 +4,10 @@
 % http://www.mathworks.com/help/simulink/ug/system-design-in-simulink-using-system-objects.html
 classdef Robot < matlab.System & matlab.system.mixin.CustomIcon
     
-    
-    % The state of the robot is a column vector containing the angular
-    % velocity of each of the four motors
+    % The state of the robot is its global location and its body velocity
     properties (ContinuousState)
-        X
+        X_g;
+        X_b_dot;
     end
     
     
@@ -42,23 +41,108 @@ classdef Robot < matlab.System & matlab.system.mixin.CustomIcon
 
         % Moment of inertia of wheel assembly.  Tau_accel = I_asm*w_dot_motor
         I_asm = 1;
-        
+
         % Max voltage applied to the motor phases (Volts)
         V = 1;
-        
+
         % Angle of each wheel axis relative to the +x axis (radians)
-        wheel_angles = (pi/4 + (pi/2)*0:3)';
+        wheel_angles = (pi/4 + (pi/2)*(0:3))';
     end
+    
+    
+
+    properties (Dependent = true)
+        J % inertial matrix
+        G % robot geometry matrix.  maps wheel velocities/forces to robot velocities/forces
+        
+        % matrices for final nonlinear state equation
+        % x_dot = A_1*x + A_2*dphidt*x + B*u
+        A_1
+        A_2
+        B
+    end
+    
+    methods
+        function J = get.J(obj)
+            J = [obj.M_bot 0 0;
+                0 obj.M_bot 0;
+                0 0 obj.I_bot];
+        end
+        
+        function G = get.G(obj)
+            G = [-sin(obj.wheel_angles(1)), -sin(obj.wheel_angles(2)), -sin(obj.wheel_angles(3)), -sin(obj.wheel_angles(4));
+                  cos(obj.wheel_angles(1)),  cos(obj.wheel_angles(2)),  cos(obj.wheel_angles(3)),  cos(obj.wheel_angles(4));
+                                   1/obj.L,                   1/obj.L,                   1/obj.L,                   1/obj.L];
+        end
+        
+        function A_1 = get.A_1(obj)
+            A_1 = - (obj.K_e*obj.K_t/obj.R + obj.K_f) / (obj.M_bot*obj.g^2*obj.r^2 + obj.I_asm) * eye(3);
+        end
+        
+        function A_2 = get.A_2(obj)
+            A_2 = - (obj.M_bot*obj.g^2*obj.r^2)/(obj.M_bot*obj.g^2*obj.r^2 + obj.I_asm) * [0, 1, 0; -1, 0, 0; 0, 0, 0];
+        end
+        
+        function B = get.B(obj)
+            B = obj.g*obj.r*obj.K_t*obj.G / obj.R / (obj.M_bot*obj.g^2*obj.r^2 + obj.I_asm);
+        end
+    end
+    
+    
+    
+    
+    methods (Access = protected)
+        % initialize at zero pose with zero velocity
+        function setupImpl(obj)
+            obj.X_g = [0 0 0]';
+            obj.X_b_dot = [0 0 0]';
+        end
+        
+        % Specify the number of inputs to the step() method.
+        % inputs: u, dt
+        function num = getNumInputsImpl(~)
+            num = 2;
+        end
+        
+        % outputs: X_g, X_b_dot
+        function num = getNumOutputsImpl(~)
+            num = 2;
+        end
+        
+        function validateInputsImpl(obj, u)
+            sz = size(u);
+            if ~(sz(1) == 4 && sz(2) == 1)
+               error('Wrong dimensions for input, should be [4, 1]') 
+            end
+        end
+        
+        function [X_b_dot, X_g] = stepImpl(obj, u, dt)
+            
+            syms X;
+            S = dsolve(diff(X) == obj.A_1*X+obj.A_2*X*([0 0 1]*X)+obj.B*u);
+            
+            % TODO
+            
+            obj.X_b_dot = [0 0 0]';
+            obj.X_g = [0 0 0]';
+            % TODO: this doesn't feel right
+            X_b_dot = obj.X_b_dot;
+            X_g = obj.X_g;
+        end
+    end
+    
     
     
     % input and output names
     methods (Access = protected)
-        function [in1name] = getInputNamesImpl(~)
+        function [in1name, in2name] = getInputNamesImpl(~)
             in1name = 'Motor Voltages';
+            in2name = 'dt';
         end
         
-        function [out1name] = getOutputNamesImpl(~)
-            out1name = 'Motor Velocities';
+        function [out1name, out2name] = getOutputNamesImpl(~)
+            out1name = 'Robot pose';
+            out2name = 'Robot velocity';
         end
     end
     
@@ -71,3 +155,4 @@ classdef Robot < matlab.System & matlab.system.mixin.CustomIcon
     end
     
 end
+
